3020c15d1baa60e307bc520d825892754463cebb
Rian Ribeiro <rianribeiroprogramador@gmail.com>
2023-01-02 21:59:01 -0300

    todo: style the carousel

--- src/components/ImageSwiper.tsx
+++ src/components/ImageSwiper.tsx
@@ -1,34 +1,31 @@
 import { Navigation } from 'swiper'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Wrapper, SwiperElement, SwiperSlider } from '../styles/ImageSwiperStyle'
 
 import 'swiper/css/bundle'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
 
 export function ImageSwiper() {
     return (
-        <Swiper
-            modules={[Navigation]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide>
-                <img src="/images/christmas.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/forest.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/new_year.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/town.jpg" />
-            </SwiperSlide>
-        </Swiper>
+        <Wrapper>
+          <SwiperElement
+              modules={[Navigation]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+          >
+              <SwiperSlider>
+                  <img src="/images/christmas.jpg" />
+              </SwiperSlider>
+              <SwiperSlider>
+                  <img src="/images/forest.jpg" />
+              </SwiperSlider>
+              <SwiperSlider>
+                  <img src="/images/new_year.jpg" />
+              </SwiperSlider>
+              <SwiperSlider>
+                  <img src="/images/town.jpg" />
+              </SwiperSlider>
+          </SwiperElement>
+        </Wrapper>
     )
 }
--- src/styles/ImageSwiperStyle.tsx
+++ src/styles/ImageSwiperStyle.tsx
@@ -1,4 +1,21 @@
 import styled from 'styled-components'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
+export const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: red;
 
+`
 
+export const SwiperElement = styled(Swiper)`
+  display: flex;
+  algin-items: center;
+  justify-content: center;
+  max-width: 20%;
+  max-height: 30;
+`
+
+export const SwiperSlider = styled(SwiperSlide)`
+  
+`
