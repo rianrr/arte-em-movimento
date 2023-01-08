1441678ff7015c9934d4d4d275f454d1c0c53ba8
Rian Ribeiro <rianribeiroprogramador@gmail.com>
2023-01-08 18:22:53 -0300

    todo: fix mission page

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -1,11 +1,25 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { Section, Image, ImageSection, ImageNd, Heading, Text, TextSection } from '../styles/MissionStyles'
 
 export function Mission() {
     return (
         <>
             <Navbar />
-            <h1>Missão</h1>
+
+            <Section>
+              <ImageSection>
+                <Image src="/images/12.JPG" />
+
+              <ImageNd src="/images/1.JPG" />
+              </ImageSection>
+              
+              <TextSection>
+                <Heading>VISÃO</Heading>
+                <Text>Fortalecer a atuação da organização social na cidade de Guarujá e ser reconhecida como uma instituição de referência nacional, apontar um caminho para um futuro com mais perspectivas e menos desigualdades sociais ao atuar como um agente de transformação social por meio da arte e da cultura, formando profissionais da dança de excelência, promovendo a inclusão social.</Text>
+              </TextSection>
+            </Section>
+
             <Footer />
         </>
     )
---
+++ src/styles/MissionStyles.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  max-width: 100vw;
+  margin: 2em 2em 2em 0em;
+`
+
+export const ImageSection = styled.div`
+  display: flex;
+  /* align-items: center; */
+  /* justify-content: center; */
+  margin: 2em 2em 2em 2em;
+`
+
+export const TextSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin-left: 10em;
+`
+
+export const Image = styled.img`
+  width: 40%;
+  height: 20em;
+  border-radius: 5em;
+  /* position: relative; */
+  /* top: 45%; */
+`
+
+export const ImageNd = styled.img`
+  width: 40%;
+  height: 20em;
+  border-radius: 5em;
+  /* position: absolute; */
+  /* left: 7%; */
+  margin-left: 2em;
+`
+
+export const Heading = styled.h1`
+  color: #f8f8f8;
+  font-size: 4em;
+  margin-bottom: .3em;
+  margin-left: 4em;
+`
+
+export const Text = styled.h3`
+  color: #f8f8f8;
+  letter-spacing: 1.5px;
+  margin-bottom: 1em;
+  text-align: center;
+`
