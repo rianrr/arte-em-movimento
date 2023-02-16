da82e5bda284e54e8de3ef542cc5db0840956a16
Rian Ribeiro <rianribeiroprogramador@gmail.com>
2023-02-16 18:23:50 -0300

    todo: portal and support content

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,11 +1,23 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
+import { Heading, Section, Image } from "../styles/PartnersStyles";
+
 export function Partners() {
   return (
     <>
       <Navbar />
-      <h1>Parceiros</h1>
+
+      <Heading>Parceiros</Heading>
+
+      <Section>
+        <Image src="/images/prefeitura.png" />
+        <Image src="/images/cmdca.png" />
+        <Image src="/images/logo_escola.png" />
+        <Image src="/images/nae.png" />
+        <Image src="/images/secretaria-de-cultura.png" />
+      </Section>
+
       <Footer />
     </>
   )
--- src/styles/PartnersStyles.tsx
+++ src/styles/PartnersStyles.tsx
@@ -1,1 +1,39 @@
 import styled from "styled-components";
+
+export const Heading = styled.h2`
+  color: #f8f8f8;
+  font-size: 3em;
+  margin: 1em 0 0 1em;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2.7em;
+  }
+`
+
+export const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: auto;
+  justify-content: center;
+  align-items: center;
+  margin: 2em 2em 2em 2em;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    flex-wrap: nowrap;
+    margin: 0;
+  }
+`
+
+export const Image = styled.img`
+  height: 10%;
+  width: 15%;
+  margin: 2em;
+
+  @media screen and (max-width: 768px) {
+    height: 40%;
+    width: 40%;
+  }
+`
