1e0d46e09cf9b05f401bcad2b232ad183d5fb257
Rian Ribeiro <rianribeiroprogramador@gmail.com>
2023-03-01 16:19:36 -0300

    Fix: Home Page Cards

--- src/pages/Portal.tsx
+++ src/pages/Portal.tsx
@@ -10,10 +10,11 @@ export function Portal() {
       <Heading>Portal da Transparência</Heading>
 
       <Section>
-        <File href="/docs/17-02-2023 1951 Microsoft Lens.pdf">Ata de Assembleia Geral Ordinária 2023</File>
-        <File href="/docs/Ata Assembleia substtituição 1ª conselheira 1.pdf" target="_blank" download>Ata de Assembleia Extraordinária 2022</File>
-        <File href="/docs/Ata de Reeleição da Diretoria.pdf" target="_blank" download>Ata de Assembleia Geral Ordinária Reeleição da Diretoria 2021</File>
-        <File href="/docs/ESTATUTO E ATA ANA ZUCCHI - COPIA CARTÓRIO.pdf" target="_blank" download>Ata de Constituição Instituto Arte em Movimento 30/10/2017 Eleição da Diretoria 2017 - 2021</File>
+        <File href="/docs/17-02-2023 1951 Microsoft Lens.pdf" target="_blank" download>Ata de Assembleia Geral Ordinária - 2023</File>
+        <File href="/docs/Ata Assembleia substtituição 1ª conselheira 1.pdf" target="_blank" download>Ata de Assembleia Extraordinária - 2022</File>
+        <File href="/docs/Balanço 2022.pdf" target="_blank" download>Balanço Patrimonial - 2022</File>
+        <File href="/docs/Ata de Reeleição da Diretoria.pdf" target="_blank" download>Ata de Assembleia Geral Ordinária Reeleição da Diretoria - 2021</File>
+        <File href="/docs/ESTATUTO E ATA ANA ZUCCHI - COPIA CARTÓRIO.pdf" target="_blank" download>Ata de Constituição Instituto Arte em Movimento 30/10/2017 Eleição da Diretoria - 2017 - 2021</File>
       </Section>
 
       <Footer />
