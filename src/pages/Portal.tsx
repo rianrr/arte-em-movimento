e15146316e180f5f7f807b91d6ca006953b4a914
Rian Ribeiro <rianribeiroprogramador@gmail.com>
2023-03-09 19:09:55 -0300

    fixed doc names

--- src/pages/Portal.tsx
+++ src/pages/Portal.tsx
@@ -10,37 +10,31 @@ export function Portal() {
       <Heading>Portal da Transparência</Heading>
 
       <Section>
-        <File href="/docs/17-02-2023-1951_Microsoft_Lens.pdf" target="_blank">
+        <File href="/docs/Ara_Assembleia_Geral_Ordinaria_2023" target="_blank">
           Ata de Assembleia Geral Ordinária - 2023
         </File>
-        <File href="/docs/Balanco_2022.pdf" target="_blank">
+        <File href="/docs/Balanco_Patrimonial_2022.pdf" target="_blank">
           Balanço Patrimonial - 2022
         </File>
         <File
-          href="/docs/Ata_Assembleia_substituicao_1_conselheira_1.pdf"
+          href="/docs/Ata_Assembleia_Extraordinaria_2022.pdf"
           target="_blank"
         >
           Ata de Assembleia Extraordinária - 2022
         </File>
-        <File
-          href="/docs/Balanco_Patrimonial_ano_fiscal_2021.pdf"
-          target="_blank"
-        >
+        <File href="/docs/Balanco_Patrimonial_2021.pdf" target="_blank">
           Balanço Patrimonial - Ano Fiscal - 2021
         </File>
         <File
-          href="/docs/Ata_de_Assembleia_Geral_Ordinaria_2022.pdf"
+          href="/docs/Ata_Assembleia_Geral_Ordinaria_2022.pdf"
           target="_blank"
         >
           Ata de Asembleia Geral Ordinária - 2022
         </File>
-        <File href="/docs/Ata_de_Reeleicao_da_Diretoria.pdf" target="_blank">
+        <File href="/docs/Ata_Reeleicao_Diretoria.pdf" target="_blank">
           Ata de Assembleia Geral Ordinária Reeleição da Diretoria - 2021
         </File>
-        <File
-          href="/docs/ESTATUTO_E_ATA_ANA_ZUCCHI_COPIA_CARTORIO.pdf"
-          target="_blank"
-        >
+        <File href="/docs/Ata_Constituicao_Eleicao.pdf" target="_blank">
           Ata de Constituição Instituto Arte em Movimento 30/10/2017 Eleição da
           Diretoria - 2017 - 2021
         </File>
